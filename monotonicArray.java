03de39df3fe07f3bd8c40fe1711a57bf18334556
Youlin <32415716+Yollyliu@users.noreply.github.com>
2018-11-25 23:55:55 -0500

    Two method to solve this problem.

---
+++ monotonicArray.java
@@ -0,0 +1,36 @@
+public class monotonicArray {
+    public static void main(String[] args) {
+
+
+    }
+    public boolean isMonotonicOne(int[] A) {
+        int store = 0;
+        for (int i = 0; i < A.length - 1; ++i) {
+            int c = Integer.compare(A[i], A[i+1]);
+            if (c != 0) {
+                if (c != store && store != 0)
+                    return false;
+                store = c;
+            }
+        }
+        return true;
+    }
+
+    public boolean isMonotonicSec(int[] A) {
+        return increasing(A) || decreasing(A);
+    }
+
+    public boolean increasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] > A[i+1]) return false;
+        return true;
+    }
+
+    public boolean decreasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] < A[i+1]) return false;
+        return true;
+    }
+
+
+}
