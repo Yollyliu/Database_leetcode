01eb2d1e0956eb643e1f913979ac4ec4b9ccb4b7
Youlin <32415716+Yollyliu@users.noreply.github.com>
2018-11-25 23:37:05 -0500

    Add files via upload

---
+++ SmallestRange1.java
@@ -0,0 +1,13 @@
+public class SmallestRange1 {
+    public static void main(String[] args) {
+
+    }
+    public int smallestRangeI(int[] A, int K) {
+        int min = A[0], max = A[0];
+        for (int x: A) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        return Math.max(0, max - min - 2*K);
+    }
+}
